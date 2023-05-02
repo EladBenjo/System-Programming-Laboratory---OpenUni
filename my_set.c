0d26d4b8efc7a108289b5b88c6f9f10d362290a4
Elad Benjo <104089989+EladBenjo@users.noreply.github.com>
2023-05-02 15:20:08 -0500

    Add files via upload

--- my_set.c
+++ my_set.c
@@ -4,23 +4,38 @@
 
 #define ENLARGE_SIZE sizeof(struct number)
 
-/**
- * This program takes users input of different numbers and creates a set.
- * Using a struct to help tracking the length of the array in order to
- * keep reallocating memory just as much needed.
- **/
+/*	This program takes in a user input of numbers and makes a set out of it.
+ *	Using a struct to track the size of the array in order to reallocate just
+ *	as much memory needed.
+ *	by Elad Benjo April 4th 2023
+ */
 
+/*function prototype*/
+struct number;
+struct number *get_set();
+void print_set(struct number *arr, int arrLength);
+
+/* this struct used to store the integer from users input and mark the last item
+	of the array*/
 struct number
 {
     int x;
     bool last;
 };
 
+/* get set is getting the users input and turning it to a 'set' using dynamic
+	memory allocating. in order to make legal set it will only store one instance
+	of the same number in the set */
 struct number *get_set()
 {
     int arrLength = 1; /* used as a "pointer" , mostly to enlrage */
     int arraySize = 0; /* used as a "pointer" , as actual pointer in the arr */
     struct number *arr = malloc(ENLARGE_SIZE);
+    if (arr == NULL)
+    {
+    	printf("Error: malloc failed to allocate memory \n");
+    	exit(EXIT_FAILURE);
+    }
     int num; /* to store users inputs */
     bool match = false; /* is this num already in the set? */
 
@@ -42,6 +57,11 @@ struct number *get_set()
         {
             arrLength++;
             arr = realloc(arr, arrLength * ENLARGE_SIZE);
+            if (arr == NULL)
+    		{
+    			printf("Error: realloc failed to allocate memory \n");
+    			exit(EXIT_FAILURE);
+   			}
             arr[arraySize].x = num;
             arr[arraySize].last = true;
             if (arraySize != 0) /* case of first number to insert */
@@ -56,13 +76,15 @@ struct number *get_set()
     return arr;
 }
 
+/* a simple print function. using the "last" property of 'struct number' it will
+	detremine printing space or new line*/
 void print_set(struct number *arr, int arrLength)
 {
     printf("\nSet contents:\n");
     for (int i = 0; i < arrLength; i++)
     {
         printf("%d", arr[i].x);
-        if (arr[i].last == 1)
+        if (arr[i].last == true)
         {
             printf("\n");
         }
@@ -73,6 +95,9 @@ void print_set(struct number *arr, int arrLength)
     }
 }
 
+/* the main function ties between get set and print set, it counts the length of
+	the array using "last" property of 'struct number' and sending it as an argument
+	for print set. */
 int main()
 {
     struct number *arr = get_set();
@@ -83,7 +108,7 @@ int main()
     }
     arrLength++;
 
-    // Print out the contents of the array
+    /* Print out the contents of the array*/
     print_set(arr, arrLength);
 
     free(arr);
